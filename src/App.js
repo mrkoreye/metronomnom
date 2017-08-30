121fd67426c1b9fd409b48a08bdecb6fd91707f6
Korey Kassir <mrkoreye@gmail.com>
2017-08-30 12:10:56 -0700

    refactored and removed metronome logic to new service

--- src/App.js
+++ src/App.js
@@ -1,104 +1,53 @@
 import React, { Component } from 'react';
-import Button from './components/button/button'
-import Cookie from './components/cookie/cookie'
+import Button from './components/button/button';
+import Cookie from './components/cookie/cookie';
+import Metronome from './services/metronome';
 import Slider from 'rc-slider';
-import BufferLoader from './buffer-loader.js';
 
 import 'rc-slider/assets/index.css';
 import './App.css';
 
-const DEFAULT_BPM = 120;
-const DEFAULT_VOLUME = 0.7;
-const VOLUME_CHANGE_INCREMENT = 0.1;
-const DEFAULT_NOTE_RESOLUTION = 4;
+const Handle = Slider.Handle;
 const MIN_BPM = 20;
 const MAX_BPM = 220;
-const ACCENT_NOTE_PLAYBACK_VALUE = 1.1;
-// Break each loop/measure up into 16 notes
-const LAST_NOTE_OF_BAR = 16;
-// in milliseconds
-const INTERVAL_FOR_TIME_CHECK = 25;
-// in seconds
-const SCHEDULE_AHEAD_TIME = 0.1;
 
-// const Howl = require('howler').Howl;
-const Handle = Slider.Handle;
-
-window.AudioContext = window.AudioContext || window.webkitAudioContext;
-const audioContext = new AudioContext();
-// The gain node is used to control volume
-const clickGainNode = audioContext.createGain();
-clickGainNode.connect(audioContext.destination);
-clickGainNode.gain.value = DEFAULT_VOLUME;
-
-// The timing aspect of this metronome is a slightly modified version of:
-// https://github.com/cwilso/metronome/blob/master/js/metronome.js
 class App extends Component {
-  current16thNoteInBar = 0;
-  notesInQueue = [];
-  timeCheckIntervalId = null;
-  clickBuffers = [];
-  nextNoteTime = 0.0;
-  lastBeatDrawn = 0;
+  metronome = null;
 
   constructor(props) {
     super(props);
-    this.toggleMetronome = this.toggleMetronome.bind(this);
+    this.metronome = new Metronome();
     this.state = {
-      metronomeOn: false,
-      bpm: DEFAULT_BPM,
+      metronomeOn: this.metronome.isOn(),
+      bpm: this.metronome.bpm(),
       currentCookieImage: 0,
-      currentClickType: 3,
-      noteResolution: DEFAULT_NOTE_RESOLUTION,
-      accentFirstBeat: false,
-    }
-
-    this.loadClickAudio();
-  }
-
-  loadClickAudio() {
-    const audioCLickFileList = [
-      'clicks/click-0.mp3',
-      'clicks/click-1.mp3',
-      'clicks/click-2.mp3',
-      'clicks/click-3.mp3',
-    ];
-
-    const bufferLoader = new BufferLoader(
-      audioContext,
-      audioCLickFileList,
-      this.initClickAudio
-    );
-  
-    bufferLoader.load();
-  }
-
-  initClickAudio = (bufferList) => {
-    for (let index = 0; index < bufferList.length; index++) {
-      const buffer = bufferList[index];
-      this.clickBuffers.push(buffer);
-    }
-  }
-
-  generateClickSource(clickNumber) {
-    const newSource = audioContext.createBufferSource();
-    const buffer = this.clickBuffers[clickNumber];
-
-    if (buffer) {
-      newSource.buffer = buffer;
-      newSource.connect(clickGainNode);
-      return newSource;
-    } else {
-      return null;
+      currentClickType: this.metronome.clickType(),
+      noteResolution: this.metronome.noteResolution(),
+      accentFirstBeat: this.metronome.accentFirstBeat(),
     }
   }
 
   toggleMetronome = () => {
-    if (this.state.metronomeOn) {
+    if (this.metronome.isOn()) {
       this.stopMetronome();
     } else {
       this.startMetronome();
     }
+  }
+
+  startMetronome = () => {
+    this.metronome.start(this.incrementCookieImage);
+    this.setState({
+      metronomeOn: this.metronome.isOn(),
+    });
+  }
+
+  stopMetronome = () => {
+    this.metronome.stop();
+    this.setState({
+      metronomeOn: this.metronome.isOn(),
+      currentCookieImage: 0,
+    });
   }
 
   incrementCookieImage = () => {
@@ -111,63 +60,10 @@ class App extends Component {
     });
   }
 
-  scheduleClick = () => {
-    while (this.nextNoteTime < audioContext.currentTime + SCHEDULE_AHEAD_TIME) {
-      this.scheduleNote(this.current16thNoteInBar, this.nextNoteTime);
-      this.advanceCurrentNote();
-    }
-  }
-
-  scheduleNote(beatNumber, time) {
-    this.notesInQueue.push({
-      note: beatNumber, time: time
-    });
-
-    // Don't play every 16th beat when we only want 8ths or 4ths
-    const shouldPlayBeat = !(beatNumber % (LAST_NOTE_OF_BAR / this.state.noteResolution));
-
-    if (shouldPlayBeat) {
-      const shouldAccentNote = beatNumber === 0 && this.state.accentFirstBeat;
-      const click = this.generateClickSource(this.state.currentClickType);
-
-      if (shouldAccentNote) {
-        click.playbackRate.value = ACCENT_NOTE_PLAYBACK_VALUE;
-      }
-
-      click.start(time);
-      setTimeout(this.incrementCookieImage, time - audioContext.currentTime)
-    }
-  }
-
-  advanceCurrentNote() {
-    const secondsPerBeat = 60.0 / this.state.bpm;
-    // 0.25 converts quarter note to 16th note
-    this.nextNoteTime += 0.25 * secondsPerBeat;
-    this.current16thNoteInBar = (this.current16thNoteInBar + 1) % LAST_NOTE_OF_BAR;
-  }
-
-  startMetronome = () => {
-    this.current16thNoteInBar = 0;
-    this.nextNoteTime = audioContext.currentTime;
-    this.timeCheckIntervalId = setInterval(this.scheduleClick, INTERVAL_FOR_TIME_CHECK);
-
-    this.setState({
-      metronomeOn: true
-    });
-  }
-
-  stopMetronome = () => {
-    clearInterval(this.timeCheckIntervalId);
-
-    this.setState({
-      metronomeOn: false,
-      currentCookieImage: 0,
-    });
-  }
-
   setBpm(newBpm) {
+    this.metronome.bpm(newBpm);
     this.setState({
-      bpm: newBpm
+      bpm: this.metronome.bpm(),
     });
   }
 
@@ -176,70 +72,42 @@ class App extends Component {
   }
 
   restartMetronome = () => {
-    if (this.state.metronomeOn) {
+    if (this.metronome.isOn) {
       this.stopMetronome();
       this.startMetronome();
     }
   }
 
   increaseVolume = () => {
-    const currentVolume = clickGainNode.gain.value;
-    const roundedValue = Math.round(currentVolume * 10) / 10;
-
-    if (roundedValue >= 1) {
-      return;
-    } else {
-      clickGainNode.gain.value = roundedValue + VOLUME_CHANGE_INCREMENT;
-    }
+    this.metronome.increaseVolume();
   }
 
   decreaseVolume = () => {
-    const currentVolume = clickGainNode.gain.value;
-    const roundedValue = Math.round(currentVolume * 10) / 10;
-
-    if (roundedValue <= 0) {
-      return;
-    } else {
-      clickGainNode.gain.value = roundedValue - VOLUME_CHANGE_INCREMENT;
-    }
+    this.metronome.decreaseVolume();
   }
 
-  nextClickType = () => {
-    const currentClick = this.state.currentClickType;
-    const nextClickInstance = (currentClick + 1) % this.clickBuffers.length;
-
+  changeClickType = () => {
+    this.metronome.clickType(true);
     this.setState({
-      currentClickType: nextClickInstance,
-    });
-  }
-
-  previousClickType = () => {
-    const currentClick = this.state.currentClickType;
-    const nextClickInstance = (currentClick - 1) >= 0 ? currentClick - 1 :  this.clickBuffers.length - 1;
-
-    this.setState({
-      currentClickType: nextClickInstance,
+      currentClickType: this.metronome.clickType(),
     });
   }
 
   setNoteResolution = (resolution) => {
-    const currentNoteResolution = this.state.noteResolution;
-
-    if (resolution && resolution !== currentNoteResolution) {
-      this.setState({
-        noteResolution: resolution
-      });
-    }
-  }
-
-  toggleAccentFirstBeat = () => {
-    const accentBeat = this.state.accentFirstBeat;
+    this.metronome.noteResolution(resolution);
     this.setState({
-      accentFirstBeat: !accentBeat
+      noteResolution: this.metronome.noteResolution(),
     });
   }
 
-  handleEl = (props) => {
+  toggleAccentFirstBeat = () => {
+    this.metronome.accentFirstBeat(true);
+    this.setState({
+      accentFirstBeat: this.metronome.accentFirstBeat()
+    });
+  }
+
+  handleEl(props) {
     const { value, dragging, index, className, ...restProps } = props;
     
     return (
@@ -270,23 +138,17 @@ class App extends Component {
               noClickAnim={true}
               addClass={'no-transition fa fa-' + (this.state.metronomeOn ? 'stop' : 'play')} 
               clickHandler={this.toggleMetronome} />
+          </div>
+          <div className="button-container">
+            <Button
+              clickHandler={this.changeClickType}
+              addClass={'click-' + this.state.currentClickType + ' click-bell fa fa-bell'} />
             <Button
               addClass="fa fa-volume-down"
               clickHandler={this.decreaseVolume} />
             <Button
               addClass="fa fa-volume-up"
               clickHandler={this.increaseVolume} />
-          </div>
-          <div className="button-container">
-            <Button
-              addClass="fa fa-arrow-left"
-              clickHandler={this.previousClickType} />
-            <Button
-              noClick={true}
-              addClass={'click-' + this.state.currentClickType + ' click-bell fa fa-bell'} />
-            <Button
-              addClass="fa fa-arrow-right"
-              clickHandler={this.nextClickType} />
           </div>
           <div className="button-container">
             <Button
---
+++ src/services/metronome.js
@@ -0,0 +1,204 @@
+import BufferLoader from './buffer-loader.js';
+
+const DEFAULT_BPM = 120;
+const DEFAULT_VOLUME = 0.7;
+const VOLUME_CHANGE_INCREMENT = 0.1;
+const DEFAULT_NOTE_RESOLUTION = 4;
+const ACCENT_NOTE_PLAYBACK_VALUE = 1.1;
+// Break each loop/measure up into 16 notes
+const LAST_NOTE_OF_BAR = 16;
+// in milliseconds
+const INTERVAL_FOR_TIME_CHECK = 25;
+// in seconds
+const SCHEDULE_AHEAD_TIME = 0.1;
+
+// Setup global AudioContext variable for different browsers
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+// The timing aspect of this metronome is a slightly modified version of:
+// https://github.com/cwilso/metronome/blob/master/js/metronome.js
+class Metronome {
+  _current16thNoteInBar = 0;
+  _timeCheckIntervalId = null;
+  _clickBuffers = [];
+  _nextNoteTime = 0.0;
+  _clickType = 3;
+  _noteResolution = DEFAULT_NOTE_RESOLUTION;
+  _accentFirstBeat = false;
+  _clickGainNode = null;
+  _audioContext = null;
+  _isOn = false;
+  _bpm = DEFAULT_BPM;
+
+  constructor() {
+    this._audioContext = new AudioContext();
+    // The gain node is used to control volume
+    this._clickGainNode = this._audioContext.createGain();
+    this._clickGainNode.connect(this._audioContext.destination);
+    this._clickGainNode.gain.value = DEFAULT_VOLUME;
+    this._loadClickAudio();
+  }
+
+  stop() {
+    this._clearInterval();
+    this._isOn = false;
+  }
+
+  start(onNoteClick) {
+    this._clearInterval();
+    this._current16thNoteInBar = 0;
+    this._nextNoteTime = this._audioContext.currentTime;
+    this._isOn = true;
+
+    this._timeCheckIntervalId = window.setInterval(() => {
+      this._scheduleClick(onNoteClick);
+    }, INTERVAL_FOR_TIME_CHECK);
+  }
+
+  isOn() {
+    return this._isOn;
+  }
+
+  bpm(newBpm) {
+    if (newBpm) {
+      this._bpm = newBpm;
+    }
+
+    return this._bpm;
+  }
+
+  clickType(cycleClick) {
+    if (cycleClick) {
+      const nextClickType = (this._clickType + 1) % this._clickBuffers.length;
+      this._clickType = nextClickType;
+    }
+
+    return this._clickType;
+  }
+
+  noteResolution(resolution) {
+    if (resolution && resolution !== this._noteResolution) {
+      this._noteResolution = resolution;
+    }
+
+    return this._noteResolution;
+  }
+
+  accentFirstBeat(toggle) {
+    if (toggle) {
+      this._accentFirstBeat = !this._accentFirstBeat;
+    }
+
+    return this._accentFirstBeat;
+  }
+
+  increaseVolume() {
+    const currentVolume = this._clickGainNode.gain.value;
+    const roundedValue = Math.round(currentVolume * 10) / 10;
+
+    if (roundedValue >= 1) {
+      return;
+    } else {
+      this._clickGainNode.gain.value = roundedValue + VOLUME_CHANGE_INCREMENT;
+    }
+  }
+
+  decreaseVolume() {
+    const currentVolume = this._clickGainNode.gain.value;
+    const roundedValue = Math.round(currentVolume * 10) / 10;
+
+    if (roundedValue <= 0) {
+      return;
+    } else {
+      this._clickGainNode.gain.value = roundedValue - VOLUME_CHANGE_INCREMENT;
+    }
+  }
+
+  _clearInterval() {
+    window.clearInterval(this._timeCheckIntervalId);
+    this._timeCheckIntervalId = null;
+  }
+
+  _loadClickAudio() {
+    const audioCLickFileList = [
+      'clicks/click-0.mp3',
+      'clicks/click-1.mp3',
+      'clicks/click-2.mp3',
+      'clicks/click-3.mp3',
+    ];
+
+    const bufferLoader = new BufferLoader(
+      this._audioContext,
+      audioCLickFileList,
+      this._initClickAudio
+    );
+  
+    bufferLoader.load();
+  }
+
+  _initClickAudio = (bufferList) => {
+    for (let index = 0; index < bufferList.length; index++) {
+      const buffer = bufferList[index];
+      this._clickBuffers.push(buffer);
+    }
+  }
+
+  _scheduleClick = (onNoteClick) => {
+    // safeguard against infinite loop
+    let loopCount = 0;
+    const loopMax = 1000;
+    const shouldScheduleNote = () => {
+      return this._nextNoteTime < (this._audioContext.currentTime + SCHEDULE_AHEAD_TIME);
+    };
+
+    while (shouldScheduleNote() && (loopCount < loopMax)) {
+      this._scheduleNote(this._current16thNoteInBar, this._nextNoteTime, onNoteClick);
+      this._advanceCurrentNote();
+      loopCount++;
+    }
+  }
+
+  _scheduleNote = (beatNumber, time, onNoteClick) => {
+    // Don't play every 16th beat when we only want 8ths or 4ths
+    const shouldPlayBeat = !(beatNumber % (LAST_NOTE_OF_BAR / this._noteResolution));
+
+    if (shouldPlayBeat) {
+      const shouldAccentNote = beatNumber === 0 && this._accentFirstBeat;
+      const click = this._generateClickSource(this._clickType);
+
+      if (shouldAccentNote) {
+        click.playbackRate.value = ACCENT_NOTE_PLAYBACK_VALUE;
+      }
+
+      click.start(time);
+
+      if (onNoteClick) {
+        // Call the callback passed in when metronome was started at the 
+        // *approx* same time as when click will happen
+        setTimeout(onNoteClick, time - this._audioContext.currentTime);
+      }
+    }
+  }
+
+  _advanceCurrentNote = () => {
+    const secondsPerBeat = 60.0 / this._bpm;
+    // 0.25 converts quarter note to 16th note
+    this._nextNoteTime += 0.25 * secondsPerBeat;
+    this._current16thNoteInBar = (this._current16thNoteInBar + 1) % LAST_NOTE_OF_BAR;
+  }
+
+  _generateClickSource = (clickNumber) => {
+    const newSource = this._audioContext.createBufferSource();
+    const buffer = this._clickBuffers[clickNumber];
+
+    if (buffer) {
+      newSource.buffer = buffer;
+      newSource.connect(this._clickGainNode);
+      return newSource;
+    } else {
+      return null;
+    }
+  }
+}
+
+export default Metronome;
